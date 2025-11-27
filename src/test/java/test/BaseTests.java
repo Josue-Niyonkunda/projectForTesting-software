3361f26bee26e8f163743b52693ac95d0fbc4c61
Josue-Niyonkunda <josueniyonkunda11@gmail.com>
2025-11-27 16:04:22 +0200

    initial commit

--- src/test/java/test/BaseTests.java
+++ src/test/java/test/BaseTests.java
@@ -1,9 +1,7 @@
 package test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class BaseTests {
     private  WebDriver driver;
     public void setUp(){
@@ -11,7 +9,7 @@ public class BaseTests {
         driver= new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         System.out.println(driver.getTitle());
-        driver.quit();
+
 
     }
     public static void main(String[] args){
