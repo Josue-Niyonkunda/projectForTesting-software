db057744d2b0d6c745591bfa27fbf037c8467dcb
Josue-Niyonkunda <josueniyonkunda11@gmail.com>
2025-12-08 17:25:04 +0200

    changes

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -13,8 +13,49 @@ public class BaseTests {
     public void setUp(){
         driver= new ChromeDriver();
         driver.get("https://candymapper.com/");
-        System.out.println(driver.getTitle());
+        System.out.println(driver.getTitle());a
         homePage=new HomePage(driver);
+        homePage.closeAds();
+    }
+}
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTests {
+    protected HomePage homePage;
+    private  WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver= new ChromeDriver();
+        driver.get("https://candymapper.com/");
+        System.out.println(driver.getTitle());a
+                homePage=new HomePage(driver);
+        homePage.closeAds();
+    }
+}
+
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTests {
+    protected HomePage homePage;
+    private  WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver= new ChromeDriver();
+        driver.get("https://candymapper.com/");
+        System.out.println(driver.getTitle());a
+                homePage=new HomePage(driver);
         homePage.closeAds();
     }
 }
